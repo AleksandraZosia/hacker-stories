9023327f402c22bc8488a4e638e90e2daa7aba40
Aleksandra Zofia <sawicka.aleksandra@aol.com>
2022-03-14 18:49:13 +0100

    One small step for a man

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 const reminder = "Order dog food!";
 
+const welcome = {
+  greeting: "Hej",
+  title: "Tato",
+};
+
+const getTitle = function (title) {
+  return title;
+};
+
 function App() {
   return (
     <div>
-      <h1> Hello World</h1>
+      <h1>
+        {" "}
+        {welcome.greeting} {getTitle(welcome.title)}
+      </h1>
+      <p> {reminder}</p>
+      <label htmlFor="search"> Search:</label>
+      <input id="search" type="text" />
     </div>
   );
 }
