348adc089a7fc6bee96757865b59fd00969faf11
Aleksandra Zofia <sawicka.aleksandra@aol.com>
2022-03-21 09:28:52 +0100

    And we journey on

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import React from "react";
 
+const useSemiPersistentState = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+  return [value, setValue];
+};
+
 const App = () => {
   const stories = [
     {
@@ -21,19 +32,26 @@ const App = () => {
     {
       title: "JQuery",
       url: "https://jquery.com/",
-      author: "Unknown",
+      author: "John Resing",
       num_comments: 2,
       points: 5,
       objectID: 2,
     },
   ];
-  const [searchTerm, setSearchTerm] = React.useState("React");
+  const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "React");
 
-  const handleSearch = (event) => setSearchTerm(event.target.value);
+  // React.useEffect(() => {
+  //   localStorage.setItem("search", searchTerm);
+  // }, [searchTerm]);
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
   const searchStories = stories.filter((story) =>
     story.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
@@ -48,36 +66,37 @@ const App = () => {
   );
 };
 
-const Search = (props) => (
-  <div>
-    <label htmlFor="search">Search:</label>
-    <input
-      id="search"
-      type="text"
-      value={props.search}
-      onChange={props.onSearch}
-    />
-  </div>
-);
+const Search = ({ search, onSearch }) => {
+  return (
+    <div>
+      <label htmlFor="search">Search:</label>
+      <input id="search" type="text" value={search} onChange={onSearch} />
+    </div>
+  );
+};
 
-const List = (props) => (
-  <ul>
-    {props.list.map((el) => (
-      <Item key={el.objectID} el={el} />
-    ))}
-  </ul>
-);
+const List = ({ list }) => {
+  return (
+    <ul>
+      {list.map(({ objectID, ...el }) => (
+        <Item key={objectID} {...el} />
+      ))}
+    </ul>
+  );
+};
 
-const Item = (props) => (
-  <li key={props.el.objectID}>
-    {" "}
-    <span>
-      <a href={props.el.url}>{props.el.title}</a>
-    </span>
-    <span>{props.el.author}</span>
-    <span>{props.el.num_comments}</span>
-    <span>{props.el.points}</span>
-  </li>
-);
+const Item = ({ title, url, author, num_comments, points }) => {
+  return (
+    <li>
+      {" "}
+      <span>
+        <a href={url}>{title}</a>
+      </span>
+      <span>{author}</span>
+      <span>{num_comments}</span>
+      <span>{points}</span>
+    </li>
+  );
+};
 
 export default App;
